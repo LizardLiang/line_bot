c56a7c4572dc041db8d57fef350ccdf7d22639c6
LIZRAD <shotup0101@gmail.com>
2019-03-12 13:23:47 +0800

    num_test_2

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    int_message = (int)message #to convert a string to a int
+    int_message = int(message) #to convert a string to a int
     if int_message % 2 == 0:
         messge = '雙數'
     else:
