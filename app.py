50fa5a6f6ac34ac32ae7a7f69eacaaa22ad0e62e
LIZRAD <shotup0101@gmail.com>
2019-03-13 19:05:20 +0800

    secret_code

--- app.py
+++ app.py
@@ -83,6 +83,7 @@ def handle_message(event):
         global game_key
         game_key = random.randint(1,1000)
         print(game_key)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("終極密碼已佈署完成")))
     if "!猜" in event.message.text:
         game_num = event.message.text.split("-")
         if int(game_num[1]) > game_key:
@@ -93,6 +94,8 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("太小了，跟你雞雞一樣")))
         elif int(game_num[1]) == game_key:
             print(game_key)
+            global game_key
+            game_key = random.randint(1,1000)
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("爆了齁，再玩啊")))
         
         
