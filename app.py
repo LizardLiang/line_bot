a6932d9697353a4e1ca99221393eb4115f725b31
LIZRAD <shotup0101@gmail.com>
2019-03-13 11:25:19 +0800

    movie

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def movie_sep(string1):
         timetable_3 = timetable_2.xpath('li')
         for cnt_1 in range(len(timetable_3)):
             result = timetable_3[cnt_1].xpath('text()')
-            reply_text += str(result)
+            reply_text += result.tostring()
             reply_text += "\n"
             print(result)
     return reply_text
