0f268be614c2f7d91c1eb2aaf4f31c7821cccaa4
LIZRAD <shotup0101@gmail.com>
2019-03-14 14:32:06 +0800

    try_temp

--- app.py
+++ app.py
@@ -125,9 +125,6 @@ def handle_message(event):
                             ]
                         )
         line_bot_api.reply_message(event.reply_token,TemplateSendMessage(alt_text="Template Example", template=button_template_message))
-    if "學弟" in event.message.text:
-        reply_text = "@林志儒"
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = reply_text))
         
         
 
