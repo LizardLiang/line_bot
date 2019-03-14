d043bd46ddeda7e90386105ba1b2a1ffee5e35aa
LIZRAD <shotup0101@gmail.com>
2019-03-14 17:17:50 +0800

    drink

--- drinks_app.py
+++ drinks_app.py
@@ -33,7 +33,7 @@ def check_drinks(drink_name):
             print("exist")
             return drinks.index(drink_name)
     print("added")
-    worksheet.append_row((json.dumps(datetime.datetime.now(), indent=4, sort_keys=True, default=str), user_id))
+    worksheet.append_row((json.dumps(datetime.datetime.now(), indent=4, sort_keys=True, default=str), drink_name))
     return -1
             
 def random_drinks():
