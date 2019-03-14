b4dbe82f8865e7fbe8dd62301d71790ab971011d
lizard <shotup0101@gmail.com>
2019-03-14 18:34:35 +0800

    movie

--- movie_app.py
+++ movie_app.py
@@ -8,7 +8,7 @@ def get_url():
     r_2 = r_1.xpath('//ul[@class=\"filmListAll2\"]')
     r_3 = r_2[0].xpath('li')
     text = ''
-    for cnt range len(r_3):
+    for cnt in range(len(r_3)):
         r_4 = r_3[cnt].xpath('a')
         text = r_4[0].xpath('text()') + '\n'
     return text
