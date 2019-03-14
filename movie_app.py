d6980a923bfe49c309e9c8f174134fe71cd9e8fd
lizard <shotup0101@gmail.com>
2019-03-14 19:18:36 +0800

    movie

--- movie_app.py
+++ movie_app.py
@@ -10,15 +10,17 @@ def get_url():
     print("r_2", etree.tostring(r_2[0]))
     r_3 = r_2[0].xpath('li')
     text = ''
+    text_url = ''
     text_1 = list()
     reply_text = ''
     for cnt in range(len(r_3)):
         print("r_3", etree.tostring(r_3[cnt]))
         r_4 = r_3[cnt].xpath('a')
         print("r_4", etree.tostring(r_4[0]))
+        text_url = etree.tostring(r_4[0])
         text = r_4[0].xpath('text()')
         text_1 += text
-        text_1 += "\n"
+        text_1 += ": " + text_url + "\n"
     reply_text = reply_text.join(text_1)
     return reply_text
         
