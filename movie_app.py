624d85aabcdbf59830a9789eadec1428b0e451d9
lizard <shotup0101@gmail.com>
2019-03-14 19:20:52 +0800

    movie

--- movie_app.py
+++ movie_app.py
@@ -20,7 +20,9 @@ def get_url():
         text_url = etree.tostring(r_4[0])
         text = r_4[0].xpath('text()')
         text_1 += text
-        text_1 = ": " + text_url + "\n"
+        text_1 += ": "
+        text_1 += text_url 
+        text_1 += "\n"
     reply_text = reply_text.join(text_1)
     return reply_text
         
