4302fafa653d162d28d4d47c025431048c180949
lizard <shotup0101@gmail.com>
2019-03-14 18:56:05 +0800

    movie

--- movie_app.py
+++ movie_app.py
@@ -6,15 +6,15 @@ def get_url():
     r = requests.get('http://www.atmovies.com.tw/movie/now/')
     r_1 = etree.HTML(r.text)
     r_2 = r_1.xpath('//ul[@class=\"filmListAll2\"]')
-    print("r_2", r_2)
+    print("r_2", etree.tostring(r_2))
     r_3 = r_2[0].xpath('li')
-    print("r_3", r_3)
+    print("r_3", etree.tostring(r_3))
     text = ''
     text_1 = list()
     reply_text = ''
     for cnt in range(len(r_3)):
         r_4 = r_3[cnt].xpath('a')
-        print("r_4", r_4)
+        print("r_4", etree.tostring(r_4))
         text = r_4[0].xpath('text()')
         text_1 += text
         text_1 += "\n"
