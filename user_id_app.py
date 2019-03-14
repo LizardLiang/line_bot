2d91ef79ff4677af97c27f09b0fa29a8b603f8cd
LIZRAD <shotup0101@gmail.com>
2019-03-14 15:10:23 +0800

    try_temp

--- user_id_app.py
+++ user_id_app.py
@@ -8,7 +8,7 @@ def check_user(user_id):
     GSpreadSheet = 'line-bot'
     while True:
         try:
-            scope = ['https://spreadsheets.google.com/feeds']
+            scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
             key = SAC.from_json_keyfile_name(GDriveJSON, scope)
             gc = gspread.authorize(key)
             worksheet = gc.open(GSpreadSheet).sheet1
