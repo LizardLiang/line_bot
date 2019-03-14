2cc4fe2c11b0c5e2b604f45f5e552c4ba7ca25f3
LIZRAD <shotup0101@gmail.com>
2019-03-14 17:19:09 +0800

    drink

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
     if "!+飲料" in event.message.text:
         drink_name = event.message.text.split("-")
-        drink_num = drinks_app.add_drinks(drink_name[1])
+        drink_num = drinks_app.add_drinks(drink_name[1], user_id)
         if drink_num == "-1":
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("新增成功")))
         elif drink_num == "0":
--- drinks_app.py
+++ drinks_app.py
@@ -15,7 +15,7 @@ def add_drinks(drink_name):
         return 0
     
     
-def check_drinks(drink_name):
+def check_drinks(drink_name, user_id):
     GDriveJSON = 'FAMAX-ef61fdf82b20.json'
     GSpreadSheet = 'line-bot'
     try:
@@ -33,7 +33,7 @@ def check_drinks(drink_name):
             print("exist")
             return drinks.index(drink_name)
     print("added")
-    worksheet.append_row((json.dumps(datetime.datetime.now(), indent=4, sort_keys=True, default=str), drink_name))
+    worksheet.append_row((json.dumps(datetime.datetime.now(), indent=4, sort_keys=True, default=str), drink_name, user_id))
     return -1
             
 def random_drinks():
