f89344af348f6d689ad048d9930a766a4fd683a7
LIZRAD <shotup0101@gmail.com>
2019-03-14 14:54:26 +0800

    try_temp

---
+++ user_id.py
@@ -0,0 +1,25 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as SAC
+users = list()
+
+def check_user(user_id):
+    GDriveJSON = 'FAMAX-ef61fdf82b20.json'
+    GSpreadSheet = 'line-bot'
+    while True:
+        try:
+            scope = ['https://spreadsheets.google.com/feeds']
+            key = SAC.from_json_keyfile_name(GDriveJSON, scope)
+            gc = gspread.authorize(key)
+            worksheet = gc.open(GSpreadSheet).sheet1
+        except Exception as ex:
+            print('無法連線Google試算表', ex)
+            sys.exit(1)
+        global users
+        users = worksheet.col_values(2)
+        print('複製已存在的users' ,GSpreadSheet)
+        if user_id in users:
+            return users.index(user_id)
+        else:
+            users.append(user_id)
+            worksheet.append_row((datetime.datetime.now(), textt))
+    
