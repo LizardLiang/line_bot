a61228a7535ae393d116eefe920670feb5a5fd4e
LIZRAD <shotup0101@gmail.com>
2019-03-15 22:07:50 +0800

    porn_app

---
+++ porn_app.py
@@ -0,0 +1,36 @@
+import gspread
+import sys
+import datetime
+import json, random
+from oauth2client.service_account import ServiceAccountCredentials
+porns = list()
+
+
+def load_porn():
+    GDriveJSON = 'FAMAX-ef61fdf82b20.json'
+    GSpreadSheet = 'line-bot'
+    try:
+        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+        key = ServiceAccountCredentials.from_json_keyfile_name(GDriveJSON, scope)
+        gc = gspread.authorize(key)
+        worksheet = gc.open(GSpreadSheet).worksheet('工作表3')
+    except Exception as ex:
+        print('無法連線Google試算表', ex)
+        sys.exit(1)
+    global porns
+    porns = worksheet.col_values(1)
+            
+def add_porn(porn_url):
+    load_porn()
+    if porn_url in porns:
+        return "exist"
+    else:
+        porns.append(porn_url)
+        worksheet.append_row((user_id))
+        print('已新增url')
+        return 'success'
+    
+def row_porn():
+    load_porn()
+    index = random.randint(0, len(porns))
+    return porns[index]
