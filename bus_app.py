3eb8d8a002191b54093f0c975274c4af50f87388
LIZRAD <shotup0101@gmail.com>
2019-03-19 17:24:22 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -8,22 +8,22 @@ def find_bus(bus_name):
     for r_3 in r_2:
         t = r_3.xpath('text()')
         try:
-            try:
-                t_4 = t.split(';')
-                print('t_4', t_4)
-                for t_5 in t_4:
-                    try:
-                        t_1 = t_5.split('ebus(')
-                    except:
-                        t_1 = t_5.split('ebus1(')
-                    print('t_1', t_1)
-                    t_2 = t_1[0].split(')')
-                    t_3 = t_2[0].split(',')
-                    print('t_3', t_3)
-            except:
-                continue
-            
-            if bus_name in str(t[0]):
-                print('bus', str(t[0]))
+            for t_6 in t:
+                try:
+                    t_4 = t_6.split(';')
+                    print('t_4', t_4)
+                    for t_5 in t_4:
+                        try:
+                           t_1 = t_5.split('ebus(')
+                        except:
+                            t_1 = t_5.split('ebus1(')
+                        print('t_1', t_1)
+                        t_2 = t_1[0].split(')')
+                        t_3 = t_2[0].split(',')
+                        print('t_3', t_3)
+                except:
+                    continue
+                if bus_name in str(t[0]):
+                    print('bus', str(t[0]))
         except:
             continue
