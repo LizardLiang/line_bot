ea788a02fda7bfbc2fff6a6f7470d30709db5b1b
LIZRAD <shotup0101@gmail.com>
2019-03-19 16:05:13 +0800

    serial-index

--- bus_app.py
+++ bus_app.py
@@ -4,7 +4,7 @@ from lxml import etree
 def find_bus(bus_name):
     r = requests.get('http://www.e-bus.gov.taipei/index_6_1.html#')
     r_1 = etree.HTML(r.text)
-    print('r', r)
+    print('r', r.text)
     r_2 = r_1.xpath('//a')
     for r_3 in r_2:
         t = r_3.text
