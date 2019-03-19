f656154fc62e6365db810c8fc43a85a4c726f784
LIZRAD <shotup0101@gmail.com>
2019-03-19 16:54:42 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -4,18 +4,12 @@ from lxml import etree
 def find_bus(bus_name):
     r = requests.get('http://www.e-bus.gov.taipei/index_6_1.html#')
     r_1 = etree.HTML(r.text)
-    print('r', r.text)
     r_2 = r_1.xpath('//script')
     for r_3 in r_2:
         t = r_3.xpath('text()')
+        print('t', t[0])
         try:
             if bus_name in t[0]:
                 print('bus', t[0])
         except:
             continue
-        try:
-            if bus_name in t2:
-                print('t_2:', t_2)
-                return t_2[1]
-        except:
-            print('none')
