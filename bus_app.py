89044b542f9174ef6249409fb48168271727c7af
LIZRAD <shotup0101@gmail.com>
2019-03-19 16:40:05 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -9,7 +9,7 @@ def find_bus(bus_name):
     for r_3 in r_2:
         t = r_3.xpath('text()')
         print('t:', t)
-        t2 = t.split(';')
+        t2 = t.index('e')
         try:
             if bus_name in t2:
                 print('t_2:', t_2)
