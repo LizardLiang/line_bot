59750def80e77e48635ba94b68bc05ba4ab4f2d5
LIZRAD <shotup0101@gmail.com>
2019-03-19 16:42:40 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -5,7 +5,7 @@ def find_bus(bus_name):
     r = requests.get('http://www.e-bus.gov.taipei/index_6_1.html#')
     r_1 = etree.HTML(r.text)
     print('r', r.text)
-    r_2 = r_1.xpath('//tr')
+    r_2 = r_1.xpath('//script')
     for r_3 in r_2:
         t = r_3.xpath('text()')
         print('t:', t)
