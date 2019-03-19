2c66bf09349b08b5db5b2e649c3779bf3a33e6b6
LIZRAD <shotup0101@gmail.com>
2019-03-19 14:50:46 +0800

    serial-index

--- app.py
+++ app.py
@@ -46,20 +46,10 @@ def handle_message(event):
     user_id = event.source.user_id
     message = TextSendMessage(text=event.message.text)
     _index = user_proccess.check_status(user_id)
-    print(_index)
-    #int_message = int(event.message.text) #to convert a string to a int
-    if event.message.text == "健":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("健三小")))
-        
-    if "!movie" == event.message.text:
-        user_proccess.set_status(user_id, 1)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str('請輸入要搜尋的電影')))
-        
-    if '!設地區' == event.message.text:
-        user_proccess.set_status(user_id, 2)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str('請輸入要搜尋的地區')))
-        
-    if _index == '1':
+    if event.message.text == '!取消' or event.message.text == '!cancel':
+        user_proccess.clear_status(user_id)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text= '取消指令'))
+    elif _index == '1':
         user_proccess.clear_status(user_id)
         cut_1 = movie_app.find_movie(user_id, event.message.text) #去尋找電影
         if 'find nothing' in cut_1:
@@ -69,22 +59,34 @@ def handle_message(event):
             if reply_text == '-1':
                 reply_text = '找不到您的電影，或是影院'
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
-            
-    if _index == '2':
+    elif _index == '2':
         user_proccess.clear_status(user_id)
         reply_text = movie_app.set_location(user_id, event.message.text)
         if reply_text == '-1':
             reply_text = '找不到您的電影，或是影院'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+    elif
+
+    
+    #int_message = int(event.message.text) #to convert a string to a int
+    if event.message.text == "健":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("健三小")))
         
-    if '!電影院' == event.message.text:
+    elif "!movie" == event.message.text:
+        user_proccess.set_status(user_id, 1)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str('請輸入要搜尋的電影')))
+        
+    elif '!設地區' == event.message.text:
+        user_proccess.set_status(user_id, 2)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str('請輸入要搜尋的地區')))
+    elif '!電影院' == event.message.text:
         reply_text = theater_app.list_theater()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = reply_text))
         
-    if "慈孤觀音" in event.message.text:
+    elif "慈孤觀音" in event.message.text:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("輕者當日，重者七日\n你要對慈孤觀音有信心")))
         
-    if "!訂票" in event.message.text:
+    elif "!訂票" in event.message.text:
         try:
             date = event.message.text.split('-')
         except ValueError:
@@ -98,7 +100,7 @@ def handle_message(event):
         else :
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=b_url))
             
-    if "!擲骰子" == event.message.text:
+    elif "!擲骰子" == event.message.text:
         if user_id == "U58e43cf60b31e2ed4a101db4cab57fa6":
             num = '6'
             text_num = "擲到的點數是: "
@@ -110,7 +112,7 @@ def handle_message(event):
             text_num += str(num)
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text_num))
             
-    if "!十八拉" == event.message.text or "!十八啦" == event.message.text:
+    elif "!十八拉" == event.message.text or "!十八啦" == event.message.text:
         num = random.randint(1,6)
         num_1 = random.randint(1,6)
         num_2 = random.randint(1,6)
@@ -119,13 +121,13 @@ def handle_message(event):
         text_num = text_num + str(num) + '、' + str(num_1) + '、' + str(num_2) + '、' + str(num_3)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text_num))
         
-    if "!終極密碼" == event.message.text:
+    elif "!終極密碼" == event.message.text:
         global game_key
         game_key = random.randint(1,1000)
         print(game_key)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("終極密碼已佈署完成")))
         
-    if "!猜" in event.message.text:
+    elif "!猜" in event.message.text:
         game_num = event.message.text.split("-")
         if int(game_num[1]) > game_key:
             print(game_key)
@@ -138,10 +140,10 @@ def handle_message(event):
             game_key = random.randint(1,1000)
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("爆了齁，再玩啊")))
             
-    if "柏翰" in event.message.text:
+    elif "柏翰" in event.message.text:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("汪汪汪")))
         
-    if "!註冊" == event.message.text:
+    elif "!註冊" == event.message.text:
         user_index = user_id_app.check_user(user_id)
         if user_index == -1:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("註冊成功")))
@@ -149,7 +151,7 @@ def handle_message(event):
             reply_text = "你是第" + str(user_index) + "位註冊的使用者"
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
             
-    if "!+飲料" in event.message.text:
+    elif "!+飲料" in event.message.text:
         drink_name = event.message.text.split("-")
         drink_num = drinks_app.add_drinks(drink_name[1], user_id)
         print("drink_num", drink_num)
@@ -158,15 +160,15 @@ def handle_message(event):
         elif drink_num == 0:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str("已存在的飲料")))
             
-    if "!抽飲料" == event.message.text:
+    elif "!抽飲料" == event.message.text:
         drink_name = drinks_app.random_drinks()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=drink_name))
         
-    if "!上映中" == event.message.text:
+    elif "!上映中" == event.message.text:
         reply_text = movie_app.get_url()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = reply_text))
         
-    if "!預告" in event.message.text:
+    elif "!預告" in event.message.text:
         movie_name = event.message.text.split('-')
         if len(movie_name) < 2 or movie_name[1] == '':
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '林志儒吃屎'))
@@ -177,7 +179,7 @@ def handle_message(event):
         else:
             reply_text = 'https://www.youtube.com/watch?v=' + teaser_url
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = reply_text))
-    if '!+番號' in event.message.text:
+    elif '!+番號' in event.message.text:
         porn_url = event.message.text.split('-')
         if len(porn_url) < 2 or porn_url[1] == '':
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '林志儒吃屎'))
@@ -186,7 +188,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '感謝老司機的帶路'))
         elif porn_status == 'exist':
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '老司機翻車拉~~'))
-    if '!抽番號' == event.message.text:
+    elif '!抽番號' == event.message.text:
         porn_url = porn_app.row_porn()
         reply_text = '老司機帶你上路: ' + porn_url
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = reply_text))
--- movie_app.py
+++ movie_app.py
@@ -7,16 +7,13 @@ def get_url():
     r.encoding = 'big-5'
     r_1 = etree.HTML(r.text)
     r_2 = r_1.xpath('//ul[@class=\"filmListAll2\"]')
-    print("r_2", etree.tostring(r_2[0]))
     r_3 = r_2[0].xpath('li')
     text = ''
     text_url = ''
     text_1 = list()
     reply_text = ''
     for cnt in range(len(r_3)):
-        print("r_3", etree.tostring(r_3[cnt]))
         r_4 = r_3[cnt].xpath('a')
-        print("r_4", etree.tostring(r_4[0]))
         text_url = etree.tostring(r_4[0])
         text = r_4[0].xpath('text()')
         text_1 += text
@@ -29,11 +26,9 @@ def get_teaser(movie_name):
     res = requests.get(url, verify=False)
     r = etree.HTML(res.text)
     r_1 = r.xpath('//a')
-    print("len(r_1)", len(r_1))
     last = None
     for entry in r_1:
         m = re.search("v=(.*)", entry.attrib['href'])
-        print("m", m)
         if m:
             target = m.group(1)
             if target == last:
@@ -41,19 +36,16 @@ def get_teaser(movie_name):
             if re.search("list", target):
                 continue
             last = target
-            print("target", target)
             return target
     return "failed"
     
 def movie_sep(_id, string1):
     t_m = string1.split(' ')
     loc_url = ''
-    print('t_m: ', t_m)
     if len(t_m) <= 0:
         loc_url = user_proccess.read_theater(_id)
     else:
         loc_url = t_m[1]
-    print('loc_url', loc_url)
     t_m_2 = loc_url.split('/')
     timetable_urL = 'http://www.atmovies.com.tw/showtime/'
     timetable_urL += t_m[0] # -> 抓到的電影網址關鍵詞
@@ -66,14 +58,12 @@ def movie_sep(_id, string1):
         timetable = timetable_text.xpath('//a[@href=\"/showtime/' + loc_url + '/\"]')
     reply_text = ""
     result_1 = list()
-    print('timetable', len(timetable))
     for cnt in range(len(timetable)):
         timetable_1 = timetable[cnt].getparent() 
         timetable_2 = timetable_1.getparent() #電影時刻的父標籤
         timetable_3 = timetable_2.xpath('li') #找到時刻的標籤
         for cnt_1 in range(len(timetable_3)):
             result = timetable_3[cnt_1].xpath('text()') #轉為 string -> 但是不知道為啥是 list
-            print('result: ', result)
             if len(result) != 0 and ' \r\n\t\t\t\t\t\t\t\t' not in result: #去掉空白的跟巨幕廳下面的換行符
                 result_1 += result #把 list 合起來
                 result_1 += "\n"
@@ -83,7 +73,6 @@ def movie_sep(_id, string1):
 def find_movie(_id, name):
     t_m = name.split(' ')
     loc_url = ''
-    print('t_m: ', t_m)
     if len(t_m) > 1:
         loc_url = theater_app.find_theater(t_m[1])
         if loc_url == '-1':
@@ -96,12 +85,10 @@ def find_movie(_id, name):
     else:
         loc_url = user_proccess.read_theater(_id)
         t_m_1 = t_m[0]
-    print('loc_url', loc_url)
     if loc_url == None:
         r_1 = requests.get('http://www.atmovies.com.tw/showtime/t02e13/a02/') #讀取樹林秀泰的網頁
     else:
         r_1 = requests.get('http://www.atmovies.com.tw/showtime/' + loc_url + '/') #讀取樹林秀泰的網頁
-        print('111111')
     r_2 = etree.HTML(r_1.text)
     r_3 = r_2.xpath('//li[@class=\"filmTitle\"]') #讀出所有電影名稱
     for cnt in range(len(r_3)):
@@ -110,7 +97,6 @@ def find_movie(_id, name):
         if t_m_1 in t_1: #比較電影名稱
             t_2 = r_4[0].attrib['href']
             t_3 = t_2.split('/')
-            print('t_3', t_3)
             return t_3[2] + ' ' + loc_url #有找到的話，回傳網址
         else:
             t_2 = 'find nothing' #沒找到的話，回傳nothing
@@ -126,7 +112,6 @@ def set_location(_id, keyword):
     _url = theater_app.find_theater(keyword)
     if _url != '-1':
         user_proccess.set_theater(_id, _url)
-        print('set_location success')
         return 'set_location success'
     else:
         return '-1'
