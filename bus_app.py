e79802e33ee1c15c368a36f252605a18e152bf18
LIZRAD <shotup0101@gmail.com>
2019-03-20 09:40:23 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -3,5 +3,4 @@ from lxml import etree
 
 def find_bus(bus_name):
     r = requests.get('https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/235?$format=JSON')
-    print('r:', r)
-    print('len(r):', len(r))
+    print('r:', r.text)
