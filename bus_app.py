79e0cb9899d29f1f7147b6fb70846ebbc34d890e
LIZRAD <shotup0101@gmail.com>
2019-03-20 09:39:08 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -2,32 +2,6 @@ import requests
 from lxml import etree
 
 def find_bus(bus_name):
-    r = requests.get('http://www.e-bus.gov.taipei/index_6_1.html#')
-    r_1 = etree.HTML(r.text)
-    r_2 = r_1.xpath('//script')
-    for r_3 in r_2:
-        t = r_3.xpath('text()')
-        try:
-            for t_6 in t:
-                try:
-                    t_4 = str(t_6).split(';')
-                    for t_5 in t_4:
-                        try:
-                           t_1 = t_5.split('(')
-                        except:
-                            t_1 = t_5.split('(')
-                        t_2 = t_1[1].split(')')
-                        t_3 = t_2[0].split(',')
-                        print('t_3', t_3)
-                        print('len(t_3[2])', len(t_3[2]))
-                        if bus_name in t_3[2]:
-                            print('bus', t_3[2])
-                            t_4 = t_3[1].split('\"')
-                            url = 'http://www.e-bus.gov.taipei/newmap/Tw/Map?rid=' + t_4[1] + '&sec=0'
-                            return url
-                            
-                except:
-                    continue
-                
-        except:
-            continue
+    r = requests.get('https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/235?$format=JSON')
+    print('r:', r)
+    print('len(r):', len(r))
