de7bd79d07317c0cd624b620837edb44b8c280e1
LIZRAD <shotup0101@gmail.com>
2019-03-20 16:28:02 +0800

    feebee

--- feebee.py
+++ feebee.py
@@ -1,10 +1,12 @@
-import requests, json, jsonparser
+import requests, json, jsonparser, re
 from lxml import etree
 
 
 def web_to_json():
     r = requests.get('https://feebee.com.tw/s/?q=AIR+PODS')
     r_1 = etree.HTML(r.text)
+    key = re.compile('//li[@class=\"pure-g items.*?\"')
+    t = r_1.xpath(key)
     r_2 = etree.tostring(r_1)
     js = jsonparser.HTMLtoJSONParser.to_json(str(r_2))
     for li in js['html']['body']['div'][4]['div'][2]['ol']['li']:
