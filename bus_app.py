0a87cb2c66ff99e37dbe36328de4c4a2d0d2c94b
LIZRAD <shotup0101@gmail.com>
2019-03-20 13:59:06 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -33,26 +33,39 @@ class Auth():
 def find_bus(bus_name, stop_name):
     url_time = 'https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/' + bus_name + '?$format=JSON'
     url_route = 'https://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/Taipei/' + bus_name + '?$format=JSON'
+    url_info = 'https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/Taipei/' + bus_name + '?$format=xml'
     a = Auth(app_id, app_key)
     # 抓預估到站時間
     response = requests.get(url_time, headers= a.get_auth_header())
     # 抓路線圖
     response_1 = requests.get(url_route, headers= a.get_auth_header())
+    #抓公車資訊
+    response_info = requests.get(url_info, headers= a.get_auth_header())
     data = json.loads(response.content)
-    data_1 = json.loads(response_1.content)
+    #data_1 = json.loads(response_1.content)
+    data_info = json.loads(response_info.content)
+    stop_1st = data_info[0]["DepartureStopNameZh"]
+    stop_last = data_info[0]["DestinationStopNameZh"]
+    print(stop_1st, stop_last)
     reply = ""
-    stops_0 = data_1[0]['Stops']
-    for stops in stops_0:
-        for d_1 in data:
+    #stops_0 = data_1[0]['Stops'] #路線圖的stops
+    reply = set_time(data, stop_name)
+    return reply
+    
+   
+
+def set_time(data, stop_name):
+    reply = ""
+    for d_1 in data:
             try:
-                if d_1['Direction'] == 0 and d_1['StopName']['Zh_tw'] == stops['StopName']['Zh_tw']:
+                if d_1['Direction'] == 0 and d_1['StopName']['Zh_tw'] == stop_name:
                     reply += 'StopName = ' + d_1['StopName']['Zh_tw'] + '(' + str(d_1['EstimateTime']) + ")min\n"
-                    print('StopName = ', d_1['StopName']['Zh_tw'], 'EST = ', d_1['EstimateTime'])
+                    return reply
             except ValueError:
                 print(ValueError)
             except :
-                print('other error', d_1)
                 if d_1['Direction'] == 0 and d_1['StopName']['Zh_tw'] == stops['StopName']['Zh_tw']:
                     reply += 'StopName = ' + d_1['StopName']['Zh_tw'] + '(公車未發車)' + "\n"
-    return reply
-   
+                    return reply
+    
+
