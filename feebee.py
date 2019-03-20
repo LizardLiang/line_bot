bbbee3899c0fa5ce30588471a31ad6e86f5de491
LIZRAD <shotup0101@gmail.com>
2019-03-20 16:09:20 +0800

    feebee

--- feebee.py
+++ feebee.py
@@ -7,5 +7,5 @@ def web_to_json():
     r_1 = etree.HTML(r.text)
     r_2 = etree.tostring(r_1)
     js = jsonparser.HTMLtoJSONParser.to_json(str(r_2))
-    for li in js['html']['body']['div'][4]['div'][0]['div'][2]['ol']['li']:
-        print(li['span'][1]['ul']['li'][''])
+    for li in js['html']['body']['div'][4]['div'][2]['ol']['li']:
+        print(li['span'][1]['div'][0]['a']['span'][1][''])
