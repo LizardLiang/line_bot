bc970432a7508a6d8333be3e6486eb56293933f9
LIZRAD <shotup0101@gmail.com>
2019-03-20 16:51:00 +0800

    bus

--- feebee.py
+++ feebee.py
@@ -6,10 +6,11 @@ def web_to_json():
     r = requests.get('https://feebee.com.tw/s/?q=AIR+PODS')
     r_1 = etree.HTML(r.text)
     t = r_1.xpath("//li[starts-with(@class,'pure-g')]")
-    t_1 = t[0]
-    t_2 = t_1.xpath('string(.)')
-    t_3 = t_2.replace('\n', '')
-    t_4 = t_3.replace(' ', '')
+    for t_1 in t:
+        t_2 = t_1.xpath('string(.)')
+        t_3 = t_2.replace('\n', '')
+        t_4 = t_3.replace(' ', '')
+        print('t', t_4)
     r_2 = etree.tostring(r_1)
     js = jsonparser.HTMLtoJSONParser.to_json(str(r_2))
-    print('t', t_4)
+  
