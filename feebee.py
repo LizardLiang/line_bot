c0c7cbf1d1d51bbb5eda8375a38676d4b34ea1ec
LIZRAD <shotup0101@gmail.com>
2019-03-20 17:04:49 +0800

    bus

--- feebee.py
+++ feebee.py
@@ -5,12 +5,17 @@ from lxml import etree
 def web_to_json():
     r = requests.get('https://feebee.com.tw/s/?q=AIR+PODS')
     r_1 = etree.HTML(r.text)
+    name = r_1.xpath('//li[starts-with(@class, "pure-g")]')
     t = r_1.xpath("//span[starts-with(@class,'price ellipsis xlarge')]|//li[starts-with(@class,'price ellipsis xlarge')]")
-    for t_1 in t:
-        t_2 = t_1.xpath('string(.)')
-        t_3 = t_2.replace('\n', '')
-        t_4 = t_3.replace(' ', '')
-        print('t', t_4)
-    r_2 = etree.tostring(r_1)
-    js = jsonparser.HTMLtoJSONParser.to_json(str(r_2))
+    for cnt in range(name):
+        name_1 = name[cnt].xpath('string(.)')
+        name_1 = name[cnt].replace('\n', '')
+        name_1 = name[cnt].replace(' ', '')
+        name_1 = name[cnt].replace('價格', '')
+        price = t[cnt].xpath('string(.)')
+        price = t[cnt].replace('\n', '')
+        price = t[cnt].replace(' ', '')
+        price = t[cnt].replace('價格', '')
+        reply = name_1 + ' ' + price
+    print(reply)
   
