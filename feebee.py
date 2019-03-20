f8d7c37967dd39a3feb81936583d9fa197b6f307
LIZRAD <shotup0101@gmail.com>
2019-03-20 17:28:23 +0800

    feebee

--- feebee.py
+++ feebee.py
@@ -8,7 +8,7 @@ def web_to_json():
     name = r_1.xpath('//li[starts-with(@class, "pure-g")]')
     t = r_1.xpath("//span[starts-with(@class,'price ellipsis xlarge')]|//li[starts-with(@class,'price ellipsis xlarge')]")
     price = ''
-    for cnt = 0 in range(len(name)-1):
+    for cnt in range(len(name)-1):
         """
         name_2 = name[cnt].xpath('span')
         name_3 = name_2[0].xpath('a')
