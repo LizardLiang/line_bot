da0233b3d30b117620be90e3a486a8e0837537d8
LIZRAD <shotup0101@gmail.com>
2019-03-20 13:12:40 +0800

    bus

--- bus_app.py
+++ bus_app.py
@@ -44,9 +44,9 @@ def find_bus(bus_name):
         for d_1 in data:
             try:
                 if d_1['Direction'] == 0 and d_1['StopName']['Zh_tw'] == stops['StopName']['Zh_tw']:
-                    reply += 'StopName = ' + str(d_1['StopName']['Zh_tw']) + 'EST = ' + str(d_1['EstimateTime']) + '\n'
+                    reply += 'StopName = ' + d_1['StopName']['Zh_tw'] + 'EST = ' + str(d_1['EstimateTime']) + '\n'
                     print('StopName = ', d_1['StopName']['Zh_tw'], 'EST = ', d_1['EstimateTime'])
             except:
-                print('d_1 =', d_1['Direction'] == 0, ', ', d_1['StopName']['Zh_tw'], 'stop_name', stops['StopName']['Zh_tw'])
+                print('d_1 =', d_1['Direction'] == 0, ', ', d_1['StopName']['Zh_tw'] == stops['StopName']['Zh_tw'])
     return reply
    
