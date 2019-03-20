09da5dd12c0afcb759ac6cc358daf4bbf0730b6c
LIZRAD <shotup0101@gmail.com>
2019-03-20 14:59:07 +0800

    feebee

--- feebee.py
+++ feebee.py
@@ -3,5 +3,6 @@ import requests, json, jsonparser
 
 def web_to_json():
     r = requests.get('https://feebee.com.tw/s/?q=AIR+PODS')
-    js = jsonparser.HTMLtoJSONParser.to_json(r.content)
+    r_1 = etree.HTML(r.text)
+    js = jsonparser.HTMLtoJSONParser.to_json(r_1)
     print(js)
