86825355149cd0f89296eb8bba9177490c45e122
LIZRAD <shotup0101@gmail.com>
2019-03-21 12:03:37 +0800

    feebee

--- feebee.py
+++ feebee.py
@@ -53,5 +53,6 @@ def web_to_json():
 def get_shorten(url):
     print('url', url)
     r = requests.post('https://api.pics.ee/v1/links/?access_token=20f07f91f3303b2f66ab6f61698d977d69b83d64', data = {'url':str(url)})
-    print(r["data"]["picseeUrl"])
-    return r["data"]["picseeUrl"]
+    r_1 = json.load(r)
+    print(r_1["data"]["picseeUrl"])
+    return r_1["data"]["picseeUrl"]
