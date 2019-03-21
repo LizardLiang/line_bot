cad79f9f8eff021038b0e42ad2e644a1471ec18a
LIZRAD <shotup0101@gmail.com>
2019-03-21 09:50:33 +0800

    feebee

--- feebee.py
+++ feebee.py
@@ -42,6 +42,7 @@ def web_to_json():
     print(len(name_l), len(price_l))
     reply = ''
     for cnt in range(len(name_l)-1):
-        reply += name_l[cnt] + price_l[cnt] + '\n'
+        reply += name_l[cnt] + ' $' +  price_l[cnt] + '\n'
     print(reply)
+    return reply
   
