8d7c5a7f86fab1f468f7273a2c371b50d5ec1a25
LIZRAD <shotup0101@gmail.com>
2019-04-20 13:27:08 +0800

    earthquake

--- earthquake.py
+++ earthquake.py
@@ -3,5 +3,5 @@ from bs4 import BeautifulSoup
 
 def e_report():
     r = requests.get("https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization=CWB-34B859A6-E135-4A75-A4F7-0C34526588BA")
-    r_1 = jsons.loads(r.content)
+    r_1 = json.loads(r.content)
     return r_1["records"]["earthquake"][1]["reportContent"]
