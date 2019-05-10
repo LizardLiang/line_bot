1fb337446b8b5afacc5f86333889a8cb0121c441
LIZRAD <shotup0101@gmail.com>
2019-05-10 13:49:48 +0800

    nana<3

---
+++ twitchapp.py
@@ -0,0 +1,14 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+def get_streams(twitchid):
+    id = twitchid.split(' ')
+    header = {'Client-ID', "tudijxjlggseb3k0gwfy8jwoiach9z"}
+    r = requests.get("https://api.twitch.tv/helix/streams?user_login=" + id[1], headers = header)
+    r_1 = json.loads(r.content)
+    r_2 = r_1["data"]
+    if len(r_2) != 0:
+        r_3 = r_2[0]
+        return id[1] + ' is currently streaming'
+    else:
+        return id[1] + ' is currently offline'
