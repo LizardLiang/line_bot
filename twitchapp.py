02c754341e47b13229a485e9d0bbe6536063e875
LIZRAD <shotup0101@gmail.com>
2019-05-10 14:02:38 +0800

    nana<3

--- twitchapp.py
+++ twitchapp.py
@@ -10,6 +10,6 @@ def get_streams(twitchid):
     if len(r_2) != 0:
         r_3 = r_2[0]
         if r_3['type'] ==  'live':
-            return id[1] + ' is currently streaming' + '\n' + 'https://www.twitch.tv/' + id[1]
+            return 'https://www.twitch.tv/' + id[1] + '\n' + id[1] + ' is currently streaming' 
     else:
         return id[1] + ' is currently offline'
