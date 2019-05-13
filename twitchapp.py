43f06269b105990c8eae9d30db91a06cc5dc69ce
LIZRAD <shotup0101@gmail.com>
2019-05-13 17:17:16 +0800

    13

--- twitchapp.py
+++ twitchapp.py
@@ -4,16 +4,27 @@ from bs4 import BeautifulSoup
 def get_streams(twitchid):
     if ' ' in twitchid:
         id = twitchid.split(' ')
+        header = {'Client-ID': "tudijxjlggseb3k0gwfy8jwoiach9z"}
+        r = requests.get("https://api.twitch.tv/helix/streams?user_login=" + id[1], headers = header)
+        r_1 = json.loads(r.content)
+        r_2 = r_1["data"]
+        if len(r_2) != 0:
+            r_3 = r_2[0]
+            if r_3['type'] == 'live':
+                return 'https://www.twitch.tv/' + id[1] + '\n' + r_3['user_name'] + '\n' + r_3['title'] 
+        else:
+            print(r_2)
+            return 'https://www.twitch.tv/' + id[1] + '\n' + id[1] + ' is currently offline'
     else:
         id = twitchid
-    header = {'Client-ID': "tudijxjlggseb3k0gwfy8jwoiach9z"}
-    r = requests.get("https://api.twitch.tv/helix/streams?user_login=" + id[1], headers = header)
-    r_1 = json.loads(r.content)
-    r_2 = r_1["data"]
-    if len(r_2) != 0:
-        r_3 = r_2[0]
-        if r_3['type'] == 'live':
-            return 'https://www.twitch.tv/' + id[1] + '\n' + r_3['user_name'] + '\n' + r_3['title'] 
-    else:
-        print(r_2)
-        return 'https://www.twitch.tv/' + id[1] + '\n' + id[1] + ' is currently offline'
+        header = {'Client-ID': "tudijxjlggseb3k0gwfy8jwoiach9z"}
+        r = requests.get("https://api.twitch.tv/helix/streams?user_login=" + id, headers = header)
+        r_1 = json.loads(r.content)
+        r_2 = r_1["data"]
+        if len(r_2) != 0:
+            r_3 = r_2[0]
+            if r_3['type'] == 'live':
+                return 'https://www.twitch.tv/' + id + '\n' + r_3['user_name'] + '\n' + r_3['title'] 
+        else:
+            print(r_2)
+            return 'https://www.twitch.tv/' + id + '\n' + id + ' is currently offline'
